c5e838961140b5bf4cc0386f2ca3ed748b821f9b
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-05-30 16:15:12 +0100

    chore[base64]: extracting authorization values

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,3 +5,13 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """inherits from Auth"""
+
+    def extract_base64_authorization_header(self, authorization_header: str)\
+            -> str:
+        '''returns the Base64 part of the Authorization header'''
+        if authorization_header is None\
+                or (not isinstance(authorization_header, str))\
+                or (not authorization_header.startswith("Basic ")):
+            return None
+        else:
+            return authorization_header.split(" ")[1]
---
+++ 0x01-Basic_authentication/tests/test_basic_auth.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+"""Test Auth class and methods"""
+import unittest
+from parameterized import parameterized
+from api.v1.auth.basic_auth import BasicAuth
+
+
+class TestAuth(unittest.TestCase):
+    """Test Auth class and methods"""
+
+    def test_require_auth_false(self):
+        """Test require"""
+        new_instance = BasicAuth()
+        self.assertEqual(new_instance.require_auth(
+            "/api/v1/status/", ["/api/v1/status/"]), False)
+
+    def test_require_auth_true_1(self):
+        """Test test_require_auth"""
+        new_instance = BasicAuth()
+        self.assertEqual(new_instance.require_auth(
+            "/api/v1/users", ["/api/v1/status/", "/api/v1/stats"]), True)
+
+    def test_require_auth_true_2(self):
+        """Test test_require_auth empt list"""
+        new_instance = BasicAuth()
+        self.assertEqual(new_instance.require_auth(
+            "/api/v1/status/", []), True)
+
+    def test_require_auth_missing_backslash(self):
+        """Test test_require_auth"""
+        new_instance = BasicAuth()
+        excld = ["/api/v1/status/", "/api/v1/users/"]
+        self.assertEqual(new_instance.require_auth(
+            "/api/v1/status", excld), False)
+        self.assertEqual(new_instance.require_auth(
+            "/api/v1/users", excld), False)
+
+    def test_require_auth_true(self):
+        """Test test_require_auth true"""
+        new_instance = BasicAuth()
+        self.assertEqual(new_instance.require_auth(
+            "/api/v1/forbidden/", ["/api/v1/status/"]), True)
+
+    def test_authorization_header_none(self):
+        """Test authorization_header"""
+        new_instance = BasicAuth()
+        self.assertEqual(new_instance.authorization_header(), None)
+
+    def test_current_user_none(self):
+        """Test current_user"""
+        new_instance = BasicAuth()
+        self.assertEqual(new_instance.current_user(), None)
+
+    @parameterized.expand([
+        (None, None),
+        (89, None),
+        ("Holberton School", None),
+        ("Basic Holberton", "Holberton"),
+        ("Basic SG9sYmVydG9u", "SG9sYmVydG9u"),
+        ("Basic SG9sYmVydG9uIFNjaG9vbA==", "SG9sYmVydG9uIFNjaG9vbA=="),
+        ("Basic1234", None),
+    ])
+    def test_extract_base64_authorization_header(self, arg, result):
+        """test extract_base64_authorization_header"""
+        new_instance = BasicAuth()
+        self.assertEqual(
+            new_instance.extract_base64_authorization_header(arg), result)
+        # self.assertEqual(
+        #     new_instance.extract_base64_authorization_header(89), None)
+        # self.assertEqual(
+        #     new_instance.extract_base64_authorization_header(
+        #         "Holberton School"), None
+        # )
+        # self.assertEqual(
+        #     new_instance.extract_base64_authorization_header(
+        #         "Basic Holberton"), "Holberton")
+
+        # self.assertEqual(
+        #     new_instance.extract_base64_authorization_header(
+        #         "Basic SG9sYmVydG9u"), "SG9sYmVydG9u")
+
+        # self.assertEqual(
+        #     new_instance.extract_base64_authorization_header(
+        #         "Basic SG9sYmVydG9uIFNjaG9vbA=="), "SG9sYmVydG9uIFNjaG9vbA==")
+
+        # self.assertEqual(
+        #     new_instance.extract_base64_authorization_header(
+        #         "Basic1234"), None)
