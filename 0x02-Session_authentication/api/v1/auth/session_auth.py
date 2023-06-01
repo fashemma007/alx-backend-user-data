ba3174a42431587373993313bf62d341b1d12fed
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-06-01 14:37:55 +0100

    chore[sessions]: logouts

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -37,3 +37,20 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(session_id)
         current_user = User.get(user_id)
         return current_user
+
+    def destroy_session(self, request=None):
+        """
+        destroy_session function
+        """
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return False
+
+        if not self.user_id_for_session_id(session_id):
+            return False
+
+        del self.user_id_by_session_id[session_id]
+        return True
