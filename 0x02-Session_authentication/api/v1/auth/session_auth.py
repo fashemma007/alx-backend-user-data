054f078560e5c6a22ee29754d50e2a92e8ee7a2d
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-06-01 08:25:22 +0100

    chore[sessions]: create user sessions

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Session authentication class handler"""
 from base64 import b64decode
-import binascii
+import uuid
 from typing import Tuple, TypeVar
 from api.v1.auth.auth import Auth
 from models.user import User
@@ -9,3 +9,17 @@ from models.user import User
 
 class SessionAuth(Auth):
     """Sessions auth handler class"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates a Session ID for a user_id:
+            * Return None if user_id is None
+            * Return None if user_id is not a string
+            * Return the Session ID if all is well
+        """
+        if user_id is None or (not isinstance(user_id, str)):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
