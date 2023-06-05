08cb2d1612126d97c847b5b9eee850723a5376e9
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-06-05 10:57:57 +0100

    feat[User]: initialize usr class

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""module docs for user.py"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User model declaration"""
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)  # the integer primary key
+    email = Column(String(250))  # a non-nullable string
+    hashed_password = Column(String(250))  # a non-nullable string
+    session_id = Column(String(250))  # a nullable string
+    reset_token = Column(String(250))  # a nullable string
