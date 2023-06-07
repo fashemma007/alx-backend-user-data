ccc1927b6f879e80e6e457fbaffa313ff172c93c
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-06-07 09:10:00 +0100

    [CHORE] Find user by session ID
    created a method that takes a single session_id string argument and returns the corresponding User or None

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -4,9 +4,33 @@ Main file
 """
 
 from auth import Auth
-session_id = '8836deab-0121-4140-ac4e-7e7d8697d95e'
+
 auth = Auth()
 
-user = auth.get_user_from_session_id(session_id)
-print(user.email)
-print(auth.create_session("unknown@email.com"))
+email = "fasogbaemma@cool.guy"
+password = "f@sogb@emm@"
+user = auth.register_user(email, password)
+valid_user = auth.valid_login(email, password)
+if valid_user:
+    print(f"Logged in as {email}")
+else:
+    print("Incorrect credentials")
+session_id = auth.create_session(user.email)
+found_user = auth.get_user_from_session_id(session_id)
+print(found_user.email)
+
+
+password = "f@@emm@"
+
+valid_user = auth.valid_login(email, password)
+if valid_user:
+    print(f"Logged in as {email}")
+else:
+    print("Incorrect credentials")
+session_id = user.session_id
+found_user = auth.get_user_from_session_id(session_id)
+print(found_user.email)
+
+session_id = 'wrongsessionid'
+found_user = auth.get_user_from_session_id(session_id)
+print(found_user.email)
