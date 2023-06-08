e3b9e4a61b22cf4858d302e4f552dada747479ce
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-06-08 09:02:55 +0100

    [CHORE] Update password end-point
    Created an end-point to update user's oassword

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -75,13 +75,28 @@ def profile():
 
 @app.route("/reset_password", methods=["POST"], strict_slashes=False)
 def get_reset_password_token():
-    """reset password endpoint"""
+    """reset password token endpoint"""
     email = request.form.get("email")
     try:
         reset_token = AUTH.get_reset_password_token(email)
     except ValueError:
         abort(403)
     return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password():
+    """reset user's password endpoint"""
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    if email is None or reset_token is None or new_password is None:
+        abort(400)
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
 
 
 if __name__ == "__main__":
